ac7eba02df9fb644dcacfb7554bde8f6e28885e3
Andrey <ScootalooRainbow@yandex.ru>
2020-11-27 23:54:46 +0900

    Push

--- frontend/src/utils/Auth.js
+++ frontend/src/utils/Auth.js
@@ -1,5 +1,5 @@
-// export const BASE_URL = 'https://api.happycabage.students.nomoreparties.xyz';
-export const BASE_URL = 'http://localhost:3000';
+export const BASE_URL = 'https://api.happycabage.students.nomoreparties.xyz';
+
 
 export const register = (email, password) => {
   return fetch(`${BASE_URL}/signup`, {
